95a76943b20a44ae1749684f2c1cbb08ea1f31c2
Mahabubur Rahman <mahabub.r753@gmail.com>
2026-01-14 03:25:35 +0600

    Dashboard Protected Logic Written

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,21 @@
-const layout = ({ children }: { children: React.ReactNode }) => {
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const user = Cookies.get("user");
+    
+    if (!user) {
+      router.replace("/");
+    }
+  }, [router]);
+
   return <div className="container w-11/12 mx-auto my-20">{children}</div>;
 };
 
-export default layout;
+export default Layout;
